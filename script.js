bb793647bab75369a550a31787764a18800c682c
talhaktkkhan <talhaghostriley@gmail.com>
2026-03-03 22:34:48 +0500

    Start implementation of fetching API

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const toggleBtn = document.getElementById('theme-toggle');
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
 });
+
+const apiurl="https://localhost:7092";
+
+const register="/api/User_Authentication/Register";
+
+const registerurl=apiurl+register;
+
+fetch(registerurl)
